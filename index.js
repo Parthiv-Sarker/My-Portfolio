b189c783dd7bbde37bf223a0dbdf683540fde425
Parthiv Sarker <sarkerparthiv@gmail.com>
2023-05-02 20:07:35 +0530

    upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function onClickMenu(){
+    document.getElementById("navbar").classList.toggle("change");
+    document.getElementById("menu").classList.toggle("close");
+    document.getElementById("close").classList.toggle("close");
+}
+
+function onClickAccordian1(){
+    document.getElementById("accordian1").classList.toggle("toggle1");
+}
+function onClickAccordian2(){
+    document.getElementById("accordian2").classList.toggle("toggle1");
+}
